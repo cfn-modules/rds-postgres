d570aecdb0311450bcbd486d1230fce36cd27ac0
Michael Wittig <michael@widdix.de>
2019-05-17 13:21:16 +0200

    add engine versions

--- test/test.js
+++ test/test.js
@@ -12,6 +12,17 @@ test.serial('defaults', async t => {
   }
 });
 
+test.serial('latest', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/latest.yml`, stackName, {}));
+    // what could we test here?
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
+
 test.serial('minimal', async t => {
   const stackName = cfntest.stackName();
   try {
