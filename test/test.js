622a97f73c8eb803fd8a379568f64e48daf70cbf
Andreas Wittig <andreas@widdix.de>
2022-11-16 09:48:25 +0100

    Adding tests

--- test/test.js
+++ test/test.js
@@ -34,6 +34,17 @@ test.serial('minimal', async t => {
   }
 });
 
+test.serial('secret', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/secret.yml`, stackName, {}));
+    // what could we test here?
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
+
 // TODO test with-alerting
 // TODO test with-hosted-zone-private
 // TODO test with-bastion (SQL access)
